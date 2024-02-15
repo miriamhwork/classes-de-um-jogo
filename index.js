1d1cd8f2041e46363d940f46792eff6af053dbe9
miriamhwork <miriamhwork@hotmail.com>
2024-02-15 19:49:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//entrada
+//nome, idade, tipo (guerreiro, mago, monge, ninja)
+
+//método
+//exibir a mensagem: "o {tipo} atacou usando {ataque}"
+//aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+//e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+//se mago -> no ataque exibir (usou magia)
+//se guerreiro -> no ataque exibir (usou espada)
+//se monge -> no ataque exibir (usou artes marciais)
+//se ninja -> no ataque exibir (usou shuriken)
+
+//saída
+//Ao final deve se exibir uma mensagem: "o {tipo} atacou usando {ataque}" 
+//ex: mago atacou usando magia ou guerreiro atacou usando espada
